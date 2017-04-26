090f586dd9590dc991a1821c2ac07488334ddf65
MisterNT <59050300@kmitl.ac.th>
2017-04-26 15:19:49 +0700

    Fix Table Database

--- user-routes.js
+++ user-routes.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-    _       = require('lodash'),
-    config  = require('./config'),
-    jwt     = require('jsonwebtoken');
+    _ = require('lodash'),
+    config = require('./config'),
+    jwt = require('jsonwebtoken');
 var passwordHash = require('password-hash');
 var pg = require('pg');
 var path = require('path');
@@ -11,83 +11,90 @@ connection.connect();
 
 pg.connect(connectionString, (err, connection, done) => {
     // Handle connection errors
-    if(err) {
+    if (err) {
         done();
         console.log(err);
         return res.status(500).json({success: false, data: err});
-    }});
+    }
+});
 
 var app = module.exports = express.Router();
 
 
 function createToken(user) {
-  return jwt.sign(_.omit(user, 'password'), config.secret, { expiresIn: 60*60*5 });
+    return jwt.sign(_.omit(user, 'password'), config.secret, {expiresIn: 60 * 60 * 5});
 }
 
 
-app.post('/register', function(req, res) {
+app.post('/register', function (req, res) {
 
-  //Validate request
-  if (!req.body.username || !req.body.password || !req.body.fullname || !req.body.age) {
-    return res.status(400).send("You must send the username, fullname, age and password");
-  }
+    //Validate request
+    if (!req.body.email || !req.body.password || !req.body.firstname || !req.body.lastname || !req.body.tel || !req.body.birthday || !req.body.gender) {
+        return res.status(400).send("You must send the email,password,firstname,lastname,tel,birthday,gender");
+    }
 
-  //Check if already exists
-  let statement = "select * from public.user where username='" + req.body.username + "'";
-  let query = connection.query(statement, function(err, result) {
-   if (result.rowCount != 0) {
-      return res.status(400).send("A user with that username already exists");
-   }
+    //Check if already exists
+    let statement = "select * from public.user where email='" + req.body.email + "'";
+    let query = connection.query(statement, function (err, result) {
+        if (result.rowCount != 0) {
+            return res.status(400).send("A user with that email already exists");
+        }
 
-    //insert into db
-    let hashedPwd = passwordHash.generate(req.body.password);
-    let statement = "insert into public.user (username, password,fullname) values ('" + req.body.username + "', '" + hashedPwd + "','"+req.body.fullname+"');"
-    let query = connection.query(statement);
+        //insert into db
+        let hashedPwd = passwordHash.generate(req.body.password);
+        let statement = "insert into public.user (email, password,firstname,lastname,tel,birthday,gender) values ('" + req.body.email + "', '" + hashedPwd + "','" + req.body.firstname + "','" + req.body.lastname + "','" + req.body.tel + "','" + req.body.birthday + "','" + req.body.gender + "');"
+        let query = connection.query(statement);
 
-    res.status(201).send({
-      success: true,
-      data: {
-        username: req.body.username,
-        fullname: req.body.fullname,
-        age: req.body.age,
-        token: createToken({"author": "CSAG \'"}),
-      }
+        res.status(201).send({
+            success: true,
+            data: {
+                username: req.body.email,
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                tel: req.body.tel,
+                birthday: req.body.birthday,
+                gender: req.body.gender,
+                token: createToken({"author": "CSAG \'"}),
+            }
+        });
+
     });
-
-  });
 });
 
 
-app.post('/login', function(req, res) {
+app.post('/login', function (req, res) {
 
-  let statement = "select * from public.user where username='" + (req.body.username) + "'";
-  var query = connection.query(statement, function(err, result) {
+    let statement = "select * from public.user where email='" + (req.body.email) + "'";
+    var query = connection.query(statement, function (err, result) {
 
-    //validate req
-    if (!req.body.username) {
-      return res.status(400).send("You must send the username");
-    }else if(!req.body.password){
-      return res.status(400).send("You must send the password");
-    }
+        //validate req
+        if (!req.body.email) {
+            return res.status(400).send("You must send the email");
+        } else if (!req.body.password) {
+            return res.status(400).send("You must send the password");
+        }
 
-    //if found username in database
-    if (result.rowCount == 0) {
-      return res.status(401).send("The username don't match");
-    }
-    //if password match
-    if (!passwordHash.verify(req.body.password, result.rows[0].password.trim())) {
-      return res.status(401).send("The  password don't match");
-    }
+        //if found username in database
+        if (result.rowCount == 0) {
+            return res.status(401).send("The username don't match");
+        }
+        //if password match
+        if (!passwordHash.verify(req.body.password, result.rows[0].password.trim())) {
+            return res.status(401).send("The  password don't match");
+        }
 
-    res.status(201).send({
-      success: true,
-      data: {
-        username: result.rows[0].username.trim(),
-        fullname: result.rows[0].fullname.trim(),
-        age: result.rows[0].age,
-        token: createToken({"author": "CSAG \'"}),
-      }
+        res.status(201).send({
+            success: true,
+            data: {
+                username: result.rows[0].email.trim(),
+                firstname: result.rows[0].firstname.trim(),
+                lastname: result.rows[0].lastname.trim(),
+                tel: result.rows[0].tel.trim(),
+                birthday: result.rows[0].birthday.trim(),
+                gender:result.rows[0].gender.trim(),
+                token: createToken({"author": "CSAG \'"}),
+            }
+        });
+
     });
-
-  });
 });
