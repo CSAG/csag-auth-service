daf33129c17635533ee08d91566f48d46507d106
MisterNT <59050300@kmitl.ac.th>
2017-04-26 14:40:41 +0700

    UpDate PgSQL

--- user-routes.js
+++ user-routes.js
@@ -2,21 +2,20 @@ var express = require('express'),
     _       = require('lodash'),
     config  = require('./config'),
     jwt     = require('jsonwebtoken');
-var mysql = require('mysql');
 var passwordHash = require('password-hash');
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'csag',
-    //multipleStatement: true
-});
-
+var pg = require('pg');
+var path = require('path');
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/csagauth';
+var connection = new pg.Client(connectionString);
 connection.connect();
 
-
-
+pg.connect(connectionString, (err, connection, done) => {
+    // Handle connection errors
+    if(err) {
+        done();
+        console.log(err);
+        return res.status(500).json({success: false, data: err});
+    }});
 
 var app = module.exports = express.Router();
 
@@ -24,7 +23,6 @@ var app = module.exports = express.Router();
 function createToken(user) {
   return jwt.sign(_.omit(user, 'password'), config.secret, { expiresIn: 60*60*5 });
 }
-
 
 
 app.post('/register', function(req, res) {
@@ -35,20 +33,16 @@ app.post('/register', function(req, res) {
   }
 
   //Check if already exists
-  let statement = 'select * from user where username=' + JSON.stringify(req.body.username) + ' LIMIT 1';
+  let statement = "select * from public.user where username='" + req.body.username + "'";
   let query = connection.query(statement, function(err, result) {
-    if (result[0] != null) {
+   if (result.rowCount != 0) {
       return res.status(400).send("A user with that username already exists");
-    }
+   }
 
     //insert into db
     let hashedPwd = passwordHash.generate(req.body.password);
-    let statement = 'insert into user (username, password) values (\"' + req.body.username + '\", \"' + hashedPwd + '\");'
-    let query = connection.query(statement, function(err, result) {
-      if(err){
-        console.log("error to insert")
-      }
-    });
+    let statement = "insert into public.user (username, password,fullname) values ('" + req.body.username + "', '" + hashedPwd + "','"+req.body.fullname+"');"
+    let query = connection.query(statement);
 
     res.status(201).send({
       success: true,
@@ -56,7 +50,7 @@ app.post('/register', function(req, res) {
         username: req.body.username,
         fullname: req.body.fullname,
         age: req.body.age,
-        id_token: createToken({"author": "babyjazz naja\'"}),
+        token: createToken({"author": "CSAG \'"}),
       }
     });
 
@@ -66,7 +60,7 @@ app.post('/register', function(req, res) {
 
 app.post('/login', function(req, res) {
 
-  let statement = 'select * from user where username=' + JSON.stringify(req.body.username) + ' LIMIT 1';
+  let statement = "select * from public.user where username='" + (req.body.username) + "'";
   var query = connection.query(statement, function(err, result) {
 
     //validate req
@@ -77,22 +71,21 @@ app.post('/login', function(req, res) {
     }
 
     //if found username in database
-    if (result[0] == null) {
-      return res.status(401).send("The username or password don't match");
+    if (result.rowCount == 0) {
+      return res.status(401).send("The username don't match");
     }
-
     //if password match
-    if (!passwordHash.verify(req.body.password, result[0].password)) {
-      return res.status(401).send("The username or password don't match");
+    if (!passwordHash.verify(req.body.password, result.rows[0].password.trim())) {
+      return res.status(401).send("The  password don't match");
     }
 
     res.status(201).send({
       success: true,
       data: {
-        username: result[0].username,
-        fullname: result[0].fullname,
-        age: result[0].age,
-        id_token: createToken({"author": "babyjazz naja\'"}),
+        username: result.rows[0].username.trim(),
+        fullname: result.rows[0].fullname.trim(),
+        age: result.rows[0].age,
+        token: createToken({"author": "CSAG \'"}),
       }
     });
 
